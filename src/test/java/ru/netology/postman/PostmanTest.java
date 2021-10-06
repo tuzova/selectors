c21bc40bcc0cc5733e4f5a3d0c356a4cd03855df
Irine Tuzova <alexandrinius@outlook.com>
2021-10-06 16:39:14 +0300

    Initial commit

---
+++ src/test/java/ru/netology/postman/PostmanTest.java
@@ -0,0 +1,26 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanTest {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Это простое решение") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Это простое решение"))
+        ;
+    }
+}
